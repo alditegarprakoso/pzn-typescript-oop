419f470cce0e1bf6e65b3921ea4a8bc6b5cc061a
Aldi Tegar Prakoso <aldiprakoso@gmail.com>
2024-07-03 10:08:55 +0700

    learn class and properties typescript

---
+++ test/class.test.ts
@@ -0,0 +1,34 @@
+describe("Class", () => {
+  class Customer {
+    readonly id: number;
+    name: string = ""; // Default Value
+    age?: number;
+
+    constructor(id: number, name: string) {
+      console.log("Create new customer");
+
+      this.id = id;
+      this.name = name;
+    }
+  }
+
+  class Order {}
+
+  it("Should can create class", () => {
+    const customer: Customer = new Customer(1, "Aldi");
+    const order: Order = new Order();
+
+    console.log(customer);
+  });
+
+  it("Should can create constructor", () => {
+    const customerOne: Customer = new Customer(2, "Tegar");
+    const customerTwo: Customer = new Customer(3, "Prakoso");
+
+    customerOne.age = 25;
+    customerTwo.age = 24 + 1;
+
+    console.log(customerOne);
+    console.log(customerTwo);
+  });
+});
